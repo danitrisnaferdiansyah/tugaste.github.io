7ebe7c848211f7dd54172f27552df449a705b310
danitrisnaferdiansyah <68495301+danitrisnaferdiansyah@users.noreply.github.com>
2020-07-21 11:13:23 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,83 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <title>Poli</title>
+  </head>
+<style type="text/css">
+
+footer {
+
+   
+    color: #777;
+  }
+
+    #footer .copyright {
+      color: #bbb;
+      font-size: 0.9em;
+      padding: 4em 2em;
+      text-align: center;
+    }
+
+      #footer .copyright li {
+        border-left: solid 1px rgba(144, 144, 144, 0.25);
+        display: inline-block;
+        list-style: none;
+        margin-left: 1.5em;
+        padding-left: 1.5em;
+      }
+
+        #footer .copyright li:first-child {
+          border-left: 0;
+          margin-left: 0;
+          padding-left: 0;
+        }
+
+    #footer a {
+      color: inherit;
+      text-decoration: none;
+    }
+
+</style>
+ 
+  <body background="pk.jpg" style="background-attachment: fixed;background-size: 100% 100%;background-repeat: no-repeat;">
+<nav class="navbar navbar-expand-lg navbar-light" style="box-shadow: 0 10px 45px rgba(0, 0, 0, 0.4);background: rgba(0,0,0,0.4);">
+  <a class="navbar-brand" href="#"><img src="pm.png" width="190" height="43"></a>
+  <a class="btn btn-outline-danger my-2 my-sm-0 ml-5" href="home.php" type="submit" onclick="alert('WELCOME TO SAPTA MARGA MEDIKA')">Continue</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+   
+    <form class="form-inline my-2 my-lg-0">
+     
+    </form>
+  </div>
+</nav>
+   
+<div class="container" style="margin: auto;">
+  <div class="row pt-4">
+    <div class="col-lg-6 pt-5" style="font-size: 150%;color:#fff;">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+      cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+      proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </div>
+
+     <div class="col-lg-6">
+     <img src="hd/kl.png" width="450" height="300">
+    </div>
+  </div>
+</div>
+
+  </body>
+</html>
